c7a76d6901609b0e38027cc0af8334ce2b5d6f49
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2025-03-05 14:08:42 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./", // Ensure correct asset paths
-  build: {
-    outDir: "dist", // Ensure Vite builds to the correct folder
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
