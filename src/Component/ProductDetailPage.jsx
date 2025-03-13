e9df952e054d356dffdb614468ed65992a682609
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2025-03-13 16:46:26 +0530

    updated

--- src/Component/ProductDetailPage.jsx
+++ src/Component/ProductDetailPage.jsx
@@ -222,6 +222,7 @@ const ProductDetails = () => {
                     width: "100%",
                     height: "200px",
                     objectFit: "cover",
+                    objectPosition: "top",
                     borderRadius: "5px"
                   }}
                 />
